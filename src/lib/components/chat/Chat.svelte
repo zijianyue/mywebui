ce93244cffd111fa84277c7c58b5c5a64c672aa0
gezijian <32694438qq.com>
2024-08-25 16:54:17 +0800

    improve audio

--- src/lib/components/chat/Chat.svelte
+++ src/lib/components/chat/Chat.svelte
@@ -1534,7 +1534,7 @@
 			await setChatTitle(_chatId, _title);
 		}
 
-		if (messages.length >= 2 && messages.at(-1).done == true) {
+		if (!$showCallOverlay && messages.length >= 2 && messages.at(-1).done == true) {
 			suggestQuestionsList = await generateChatSuggestQuestions(messages)
 			console.log('suggestQuestionsList:', suggestQuestionsList);
 		}
@@ -1812,7 +1812,7 @@
 			await setChatTitle(_chatId, _title);
 		}
 
-		if (messages.length >= 2 && messages.at(-1).done == true) {
+		if (!$showCallOverlay && messages.length >= 2 && messages.at(-1).done == true) {
 			suggestQuestionsList = await generateChatSuggestQuestions(messages)
 			console.log('suggestQuestionsList for openai:', suggestQuestionsList);
 		}
