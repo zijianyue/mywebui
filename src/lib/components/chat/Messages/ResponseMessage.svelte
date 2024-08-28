27dbcbea6fb4e0683ff835663d4e0ca3a2d4e917
gezijian <32694438qq.com>
2024-08-28 18:31:47 +0800

    add missing code

--- src/lib/components/chat/Messages/ResponseMessage.svelte
+++ src/lib/components/chat/Messages/ResponseMessage.svelte
@@ -294,7 +294,9 @@ A：回答2`;
 	const toggleSpeakMessage = async () => {
 		if (speaking) {
 			try {
-				speechSynthesis.cancel();
+				if ($config.audio.tts.engine === '') {
+					speechSynthesis.cancel();
+				}
 
 				if (speakingIdx !== undefined && audioParts[speakingIdx]) {
 					audioParts[speakingIdx]!.pause();
@@ -486,7 +488,6 @@ A：回答2`;
 		recentMessages = '';
 		if (isLastMessage && message.done) {
 			recentMessages = getHistoryPromptText(messages);
-			console.debug('recentMessages:', recentMessages);
 		}
 	})();
 
