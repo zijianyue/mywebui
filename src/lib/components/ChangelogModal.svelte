24b05be2aef7563d10af8fd328676fc183458ed1
gezijian <32694438qq.com>
2024-08-28 23:39:25 +0800

    change localhost url to public url

--- src/lib/components/ChangelogModal.svelte
+++ src/lib/components/ChangelogModal.svelte
@@ -66,17 +66,17 @@
 						<span class="text-lg font-medium text-gray-800">跳转其他板块</span>
 						<div class="flex flex-col sm:flex-row sm:flex-wrap -mx-1">
 							<div class="px-1 mb-2">
-								<a href="http://localhost:3000/" target="_blank" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-gradient-to-r from-green-600 to-blue-600 hover:from-green-700 hover:to-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+								<a href="https://dify.nas.cpolar.cn" target="_blank" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-gradient-to-r from-green-600 to-blue-600 hover:from-green-700 hover:to-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
 									<span>{$i18n.t('工作流和智能体')}</span>
 								</a>
 							</div>
 							<div class="px-1 mb-2">
-								<a href="http://localhost:7860/" target="_blank" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-gradient-to-r from-purple-600 to-blue-600 hover:from-purple-700 hover:to-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+								<a href="https://llamafactory.nas.cpolar.cn" target="_blank" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-gradient-to-r from-purple-600 to-blue-600 hover:from-purple-700 hover:to-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
 									<span>{$i18n.t('模型训练（量化和跑分暂无界面）')}</span>
 								</a>
 							</div>
 							<div class="px-1 mb-2">
-								<a href="http://localhost:8188/" target="_blank" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-gradient-to-r from-red-600 to-orange-600 hover:from-red-700 hover:to-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500">
+								<a href="https://comfyui.nas.cpolar.cn" target="_blank" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-gradient-to-r from-red-600 to-orange-600 hover:from-red-700 hover:to-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500">
 									<span>{$i18n.t('ComfyUI生成图片')}</span>
 								</a>
 							</div>
