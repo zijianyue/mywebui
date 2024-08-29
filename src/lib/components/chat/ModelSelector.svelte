a17449bca88c56e1dc7ac7b3fee012978354d708
gezijian <32694438qq.com>
2024-08-29 14:31:17 +0800

    i18n and restore multi model select for user

--- src/lib/components/chat/ModelSelector.svelte
+++ src/lib/components/chat/ModelSelector.svelte
@@ -101,20 +101,11 @@
 								stroke="currentColor"
 								class="size-3"
 							>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke-width="2"
-									stroke="currentColor"
-									class="size-3.5"
-								>
-									<path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12h-15" />
-								</svg>
-							</button>
-						</Tooltip>
-					</div>
-				{/if}
+								<path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12h-15" />
+							</svg>
+						</button>
+					</Tooltip>
+				</div>
 			{/if}
 		</div>
 	{/each}
