c7d640a307644dae71ae38c134e333f44957541f
gezijian <32694438qq.com>
2024-08-29 14:31:17 +0800

    i18n and restore multi model select for user

--- src/lib/components/chat/Messages/ResponseMessage.svelte
+++ src/lib/components/chat/Messages/ResponseMessage.svelte
@@ -879,9 +879,9 @@ A：回答2`;
 								{#if message.info}
 									<Tooltip
 										content={message.info.openai
-											? `prompt_tokens: ${message.info.prompt_tokens ?? 'N/A'}<br/>
-													completion_tokens: ${message.info.completion_tokens ?? 'N/A'}<br/>
-													total_tokens: ${message.info.total_tokens ?? 'N/A'}`
+											? `输入的tokens: ${message.info.prompt_tokens ?? 'N/A'}<br/>
+													输出的tokens: ${message.info.completion_tokens ?? 'N/A'}<br/>
+													总计: ${message.info.total_tokens ?? 'N/A'}`
 											: `response_token/s: ${
 													`${
 														Math.round(
