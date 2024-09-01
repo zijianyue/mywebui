10e153fb7b3def23f8d054fd91c4e37a6590122f
gezijian <32694438qq.com>
2024-09-01 17:02:16 +0800

    fix image switch

--- src/lib/components/chat/Chat.svelte
+++ src/lib/components/chat/Chat.svelte
@@ -2269,6 +2269,7 @@
 					bind:autoScroll
 					bind:selectedToolIds
 					bind:webSearchEnabled
+					bind:generateImageEnabled
 					bind:atSelectedModel
 					availableToolIds={selectedModelIds.reduce((a, e, i, arr) => {
 						const model = $models.find((m) => m.id === e);
