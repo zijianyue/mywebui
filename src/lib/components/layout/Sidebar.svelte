a0c4e291eca41428bdf640c328eca1e5cfc99137
gezijian <32694438qq.com>
2024-09-12 18:01:45 +0800

    allow comfyui access for everyone from cpoe

--- src/lib/components/layout/Sidebar.svelte
+++ src/lib/components/layout/Sidebar.svelte
@@ -621,7 +621,6 @@
 					<span>{$i18n.t('模型训练微调')}</span>
 				</a>
 				<a href="https://comfyui.nas.cpolar.cn" target="_blank"
-				   on:click={(event) => handleModuleUIClickWrapper(event, 'https://comfyui.nas.cpolar.cn')}
 					class="custom-button comfy-ui-button">
 					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
 						<rect x="2" y="2" width="20" height="20" rx="2.18" ry="2.18"/>
