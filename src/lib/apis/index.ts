0ce26f96706d514fd967c0b90dc47b1417490919
gezijian <32694438qq.com>
2024-09-12 20:04:08 +0800

    replace gemma27b to siliconflow's

--- src/lib/apis/index.ts
+++ src/lib/apis/index.ts
@@ -31,7 +31,7 @@ export const getModels = async (token: string = '') => {
 		.filter((model) => {
 			// console.log(`Model: id=${model.id}, urlIdx=${model.urlIdx}`);
 			// 临时处理
-			const cond4Gemma27b = !(model.id === 'google/gemma-2-27b-it' && model.urlIdx === 2);
+			const cond4Gemma27b = !(model.id === 'google/gemma-2-27b-it' && model.urlIdx === 4);
 			const cond4Gemini = model.urlIdx !== 6 || (model.urlIdx === 6 && model.id.toLowerCase().includes('gemini-1.5'));
 
 			return cond4Gemma27b && cond4Gemini;
